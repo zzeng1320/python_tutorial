50a840777b1cc69f63bab81ce12f663bb00b007d
Zhen Zeng <zzeng@ucar.edu>
2020-10-14 13:47:34 -0600

    Adding script file

---
+++ mysci.py
@@ -0,0 +1,17 @@
+# Read the data file
+filename = "data/wxobs20170821.txt"
+
+with open(filename, 'r') as datafile:
+    data = datafile.read()
+
+#datafile = open(filename, 'r')
+
+#print(datafile.readline())
+#print(datafile.readline())
+#print(datafile.readline())
+#print(datafile.readline())
+
+#data = datafile.read()
+
+#datafile.close()
+
