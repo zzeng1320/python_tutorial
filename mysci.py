f227b9390a2d8f1c22299c36eb07e0ff013117c0
Zhen Zeng <zzeng@ucar.edu>
2020-11-11 13:47:36 -0700

    Output formatting comparison data

--- mysci.py
+++ mysci.py
@@ -1,8 +1,8 @@
 # Column names and column indices to read
-columns = {'date': 0, 'time': 1, 'tempout': 2, 'windspeed': 7}
+columns = {'date': 0, 'time': 1, 'tempout': 2, 'windspeed': 7, 'windchill': 12}
 
 # Data types for each column (only if non-string)
-types = {'tempout': float, 'windspeed': float}
+types = {'tempout': float, 'windspeed': float, 'windchill': float}
 
 # Initialize my data variable
 data = {}
@@ -42,4 +42,13 @@ def compute_windchill(t, v):
 windchill = []
 for temp, windspeed in zip(data['tempout'], data['windspeed']):
     windchill.append(compute_windchill(temp, windspeed))
-print(windchill)
+
+# Output comparison of data
+print('                ORIGINAL  COMPUTED           ')
+print(' DATE    TIME  WINDCHILL WINDCHILL DIFFERENCE')
+print('------- ------ --------- --------- ----------')
+zip_data = zip(data['date'], data['time'], data['windchill'], windchill)
+for date, time, wc_orig, wc_comp in zip_data:
+    wc_diff = wc_orig - wc_comp
+    print(f'{date} {time:>6} {wc_orig:9.6f} {wc_comp:9.6f} {wc_diff:10.6f}')
+
