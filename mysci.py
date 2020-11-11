0bcdd45e46447968079e0deef02affd5b3a99170
Zhen Zeng <zzeng@ucar.edu>
2020-11-11 13:12:15 -0700

    Reading windspeed in too

--- mysci.py
+++ mysci.py
@@ -1,17 +1,30 @@
-# Read the data file
+# Column names and column indices to read
+columns = {'date': 0, 'time': 1, 'tempout': 2, 'windspeed': 7}
+
+# Data types for each column (only if non-string)
+types = {'tempout': float, 'windspeed': float}
+
+# Initialize my data variable
+data = {}
+for column in columns:
+   data[column] = []
+
+# Read and parse the data file
 filename = "data/wxobs20170821.txt"
-
 with open(filename, 'r') as datafile:
-    data = datafile.read()
 
-#datafile = open(filename, 'r')
+   # Read the first three lines (header)
+   for _ in range(3):
+      datafile.readline()
 
-#print(datafile.readline())
-#print(datafile.readline())
-#print(datafile.readline())
-#print(datafile.readline())
+   # Read and parse the rest of the file
+   for line in datafile:
+      split_line = line.split()
+      for column in columns:
+         i = columns[column]
+         t = types.get(column, str)
+         value = t(split_line[i])
+         data[column].append(value)
 
-#data = datafile.read()
-
-#datafile.close()
-
+# DEBUG
+#print(data['tempout'])
