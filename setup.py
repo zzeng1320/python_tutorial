619753d416e4a6aad4d78599d5b792c81c5c2979
Zhen Zeng <zzeng@ucar.edu>
2021-02-24 12:30:13 -0700

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+
+setup(
+    name="mysci",
+    version="1.0.0",
+    description="A sample package",
+    author="Xdev",
+    author_email="Xdev@ucar.edu",
+    packages=["mysci"],
+    install_requires=[])
